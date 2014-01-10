d1079b93c4b81c21e0fa00c022c5058d15761435
birtles <birtles@gmail.com>
2014-01-10 22:25:52 +0900

    Add basic test framework for core component

---
+++ src/core/test/sync.js
@@ -0,0 +1,8 @@
+define(['core/sync', 'qunit'], function(Sync, QUnit) {
+  QUnit.module("Sync tests");
+  test("Test ctor", function() {
+    var sync = new Sync("a", "b");
+    ok(sync.username, "a");
+    ok(sync.password, "b");
+  });
+});
